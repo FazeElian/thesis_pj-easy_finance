bad434eed967b27fb711bef8840af8335b4ccd8d
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-09-07 10:52:38 -0500

    Adjusting text container

--- src/views/Glosary/BasicConceptsView.jsx
+++ src/views/Glosary/BasicConceptsView.jsx
@@ -51,17 +51,16 @@ const BasicConceptsView = () => {
           </div>
           <div className="body--glosary flex-column">
             <img src={Saving} alt="" />
-            <div className="body-group--glosary body-group-big--glosary">
-              <button className="btn-audio--glosary bg-yellow" onClick={handleSound}>
-                <img src={ListenAudioIcon} alt="" />
-              </button>
-              <h2>Ahorrar</h2>
-            </div>
-            <div className="body-group--glosary body-group-small--glosary">
-              <button className="btn-audio--glosary bg-yellow">
-                <img src={ListenAudioIcon} alt="" />
-              </button>
-              <h2>Es guardar parte del dinero para usarlo en el futuro. Ayuda a alcanzar metas importantes en lugar de gastarlo todo de inmediato.</h2>
+            <div className="body-group--glosary">
+              <div className="content-body-group--glosary">
+                <button className="btn-audio--glosary bg-yellow" onClick={handleSound}>
+                  <img src={ListenAudioIcon} alt="" />
+                </button>
+                <div className="text-body--glosary">
+                  <h2>Ahorrar</h2>
+                  <p>Es guardar parte del dinero para usarlo en el futuro. Ayuda a alcanzar metas importantes en lugar de gastarlo todo de inmediato.</p>
+                </div>
+              </div>
             </div>
           </div>
         </main>
