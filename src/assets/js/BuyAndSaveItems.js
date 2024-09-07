fb470f02298673b6497cc330ee21717d8e3b7ac4
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-09-07 18:40:17 -0500

    Creation of Buy and Save game view

---
+++ src/assets/js/BuyAndSaveItems.js
@@ -0,0 +1,44 @@
+const Items = [
+    {
+        id: "1",
+        name: "Product 1",
+        price: "1.000 $"
+    },
+    {
+        id: "2",
+        name: "Product 2",
+        price: "2.000 $"
+    },
+    {
+        id: "3",
+        name: "Product 3",
+        price: "3.000 $"
+    },
+    {
+        id: "4",
+        name: "Product 4",
+        price: "4.000 $"
+    },
+    {
+        id: "5",
+        name: "Product 5",
+        price: "5.000 $"
+    },
+    {
+        id: "6",
+        name: "Product 6",
+        price: "6.000 $"
+    },
+    {
+        id: "7",
+        name: "Product 7",
+        price: "7.000 $"
+    },
+    {
+        id: "8",
+        name: "Product 8",
+        price: "8.000 $"
+    },
+];
+
+export default Items;
--- src/views/BuyAndSaveView.jsx
+++ src/views/BuyAndSaveView.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 // Header component
 import Header from '../components/Header'
 
+// Styles for this component
+import "../assets/sass/views/BuyAndSaveView.scss";
+
+// Images - icons
+  // People
+  import People from "../assets/img/BuyAndSaveView/people.png";
+
+// Items
+import Items from '../assets/js/BuyAndSaveItems';
+
 const BuyAndSaveView = () => {
   return (
     <>
@@ -13,8 +23,48 @@ const BuyAndSaveView = () => {
         bderColor="bder-blue-3"
       />
       
-      <main className="py-content">
-        <h1>Buy and Save View</h1>
+      <main className="py-top">
+        <section className="bg--buy-and-save">
+          <div className="cont-img--buy-and-save">
+            <img src={People} alt="" />
+          </div>
+          <div className="cont-list-prods--buy-and-save">
+            <div className="list-prods--buy-and-save flex-column">
+              <div className="title-list-prods--buy-and-save">
+                <h1>Lista de <br /> Compras</h1>
+              </div>
+              <table className="table-prods--buy-and-save flex-column">
+                <thead className="th-table-prods--buy-and-save">
+                  <tr className="table-prods-w-75">Producto</tr>
+                  <tr className="table-prods-w-25">Precio</tr>
+                </thead>
+
+                {/* Items */}
+                {Items.map ((item) => (
+                  <tbody className="tb-table-prods--buy-and-save" key={item.id}>
+                    <td className="tb-item-table-prods--buy-and-save">
+                      <div className="table-prods-w-75">
+                        <input type="checkbox" name={item.id} id="slct-prod" />
+                        {item.name} 
+                      </div>
+                      <div className="table-prods-w-25">
+                        {item.price}
+                      </div>
+                    </td>
+                  </tbody>
+                ))}
+              </table>
+
+              <div className="balance--buy-and-save">
+                <h2>Saldo: <h3>10000 $</h3></h2>
+              </div>
+
+              <div className="cont-btn-pay--buy-and-save">
+                <button className="btn-pay--buy-and-save">Pagar</button>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   )
