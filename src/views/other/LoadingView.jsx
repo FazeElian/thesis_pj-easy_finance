c01aea2669cbed614f868f9f46aec2ed455cac4f
FazeElian <elian.ibarra@soy.sena.edu.co>
2024-09-07 16:28:29 -0500

    Creation of Loading view with React Spinners librarie

--- src/views/other/LoadingView.jsx
+++ src/views/other/LoadingView.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+// React spinner components
+import { Hourglass } from 'react-loader-spinner'
+
 const LoadingView = () => {
   return (
-    <div>LoadingView</div>
+    <main className="content-centered w-100 h-100 flex-column loading">
+      <Hourglass
+        visible={true}
+        height="110"
+        width="110"
+        ariaLabel="hourglass-loading"
+        colors={['#F25260', '#F299A9']}
+      />
+    </main>
   )
 }
 
